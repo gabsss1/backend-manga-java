aeff253b8db6fbcdb9ceb5e08fb1b41c1c31deb1
gabsss1 <gventuram2015@gmail.com>
2024-12-03 15:57:05 -0500

    backend

---
+++ src/main/java/com/example/manga_backend/MangaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.manga_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manga_backend/MangaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manga_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
